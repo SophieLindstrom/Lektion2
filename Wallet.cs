cc6e3d58d9338121c0e3f24b9fc08d08c932c613
Sophie Lindström <sophie.lindstrom90@gmail.com>
2021-11-26 15:05:46 +0100

    Add project files.

---
+++ Wallet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lektion2
+{
+    public class Wallet
+    {
+        // Mängden pengar i plånboken, får aldrig vara negativt
+        private Kronor amount;
+
+        // Skapar en tom plånbok
+        public Wallet()
+        {
+
+        }
+
+        // Skapar en plånbok med pengar i
+        public Wallet(Kronor money)
+        {
+            Add(money);
+        }
+
+        /* 
+         * Lägger till pengar till plånboken.
+         */
+        public void Add(Kronor money)
+        {
+            amount = amount.Add(money);
+        }
+
+        /*
+         * Tar pengar ur plånboken
+         * Det ska inte gå att ta ut mer pengar än vad som finns i plånboken
+         */
+        public void Remove(Kronor money)
+        {
+            amount = amount.Add(money);
+        }
+
+        /*
+         * Tar bort alla pengar ur plånboken
+         */
+        public void RemoveAll()
+        {
+            amount = new Kronor();
+        }
+    }
+}
